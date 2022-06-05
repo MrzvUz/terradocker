71eaf55b3df1f3e4ecca6ea33963d2eae083fbf8
mrzvuz <mrzv.uz@gmail.com>
2022-06-04 21:18:38 -0500

    Added outputs, spat operator, join function, count.index

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
-resource "docker_image" "nodered_imag" {
+resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  count   = 1
+  length  = 4
+  upper   = false
+  special = false
+}
+
+resource "docker_container" "nodered_container" {
+  count = 1
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = var.internal_port
+    # external = var.external_port
+  }
+}
+
+output "ip_address" {
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP address and external port of the container"
+}
+
+output "container_name" {
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the container"
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "internal_port" {
+  type        = number
+  default     = 1880
+  description = "Nodered internal port"
+}
+
+variable "external_port" {
+  type        = number
+  default     = 1880
+  description = "Nodered external port"
+}
