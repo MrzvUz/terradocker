3db6b490bd2c2c023bb829474ddb16655f02a48c
mrzvuz <mrzv.uz@gmail.com>
2022-06-05 21:24:06 -0500

    Added outputs.tf variables.tf terraform.tfvars

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && chown -R 1000:1000 noderedvol/"
+  }
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  count   = 1
+  count   = var.container_count
   length  = 4
   upper   = false
   special = false
 }
 
 resource "docker_container" "nodered_container" {
-  count = 1
+  count = var.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = var.internal_port
-    # external = var.external_port
+    external = var.external_port
   }
-}
-
-output "ip_address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "The IP address and external port of the container"
-}
-
-output "container_name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
+  volumes {
+    container_path = "/data"
+    host_path = "/Users/akhmatali_mirzaev/Documents/Projects/terradocker/noderedvol"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,22 @@
+variable "container_count" {
+  type = number
+}
+
 variable "internal_port" {
   type        = number
-  default     = 1880
-  description = "Nodered internal port"
+
+  validation {
+    condition = var.internal_port == 1880
+    error_message = "The internal port must be 1880."
+  }
 }
 
 variable "external_port" {
   type        = number
-  default     = 1880
-  description = "Nodered external port"
+
+  validation {
+    condition = var.external_port <= 65535 && var.external_port > 0
+    error_message = "The external port must be in the valid port range 0 - 65535."
+  }
 }
+
