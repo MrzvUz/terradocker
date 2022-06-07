0ee0c29245b071f68aaebee1e8678dd5ae84caf1
mrzvuz <mrzv.uz@gmail.com>
2022-06-07 10:01:38 -0500

    Added nodredvol to .ignore and added path.cwd string interpolation to provide the current directory path

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
-    command = "mkdir noderedvol/ || true && chown -R 1000:1000 noderedvol/"
+    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
   }
 }
 
@@ -9,22 +9,22 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
-  count   = var.container_count
+  count   = local.container_count
   length  = 4
   upper   = false
   special = false
 }
 
 resource "docker_container" "nodered_container" {
-  count = var.container_count
+  count = local.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = var.internal_port
-    external = var.external_port
+    external = var.external_port[count.index]
   }
   volumes {
     container_path = "/data"
-    host_path = "/Users/akhmatali_mirzaev/Documents/Projects/terradocker/noderedvol"
+    host_path      = "${path.cwd}/noderedvol" # path.cwd with string interpolation will provide the full path for the current directory.
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
 variable "container_count" {
-  type = number
+  type    = number
+  default = 4
 }
 
 variable "internal_port" {
-  type        = number
+  type = number
 
   validation {
-    condition = var.internal_port == 1880
+    condition     = var.internal_port == 1880
     error_message = "The internal port must be 1880."
   }
 }
 
 variable "external_port" {
-  type        = number
+  type = list(any)
 
   validation {
-    condition = var.external_port <= 65535 && var.external_port > 0
+    condition     = max(var.external_port...) <= 65535 && min(var.external_port...) > 0 # "..." is a spread operator when we use lists.
     error_message = "The external port must be in the valid port range 0 - 65535."
   }
 }
